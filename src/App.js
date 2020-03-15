ecd27fcadd7d079e0a84f20561d43a9f62dbe2e1
Ubuntu <ubuntu@ip-172-31-95-114.ec2.internal>
2020-03-15 18:00:39 +0000

    added changes in feature branch

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, request <code>Soumyojit</code> to save and reload.
+          To deploy changes from feature branch, request <code>Soumyojit</code> to save and load the changes.
         </p>
       </div>
     );
